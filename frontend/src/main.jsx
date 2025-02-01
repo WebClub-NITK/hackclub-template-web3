0c5a1147f8fb8e7b47932731a6d5c8b3b8d09fd9
Shivam-kum-mhta <sk8625228@gmail.com>
2025-02-02 01:28:44 +0530

    Used Web3Modal & wagmi for Multiple Wallet & Network connection

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,39 +4,9 @@ import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-  getDefaultWallets,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { mainnet, sepolia } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
-
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [sepolia],
-  [publicProvider()]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: 'Web3 Hackathon Starter',
-  projectId: 'YOUR_PROJECT_ID',
-  chains,
-});
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-  webSocketPublicClient,
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        <App />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <App />
   </React.StrictMode>
 );
